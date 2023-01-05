59add1e685954b7e671371bf45539c31b85445c1
dwaynediddy <burrows.dwayne@icloud.com>
2023-01-05 11:52:59 +1000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dwayne on 4/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
