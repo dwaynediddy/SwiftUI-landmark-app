23b81cf6e2fa4d5428d467b06946765e6572f18f
dwaynediddy <burrows.dwayne@icloud.com>
2023-01-11 09:09:41 +1000

    adding hike data to application

---
+++ Landmarks/Views/HikeView.swift
@@ -0,0 +1,20 @@
+//
+//  HikeView.swift
+//  Landmarks
+//
+//  Created by Dwayne on 10/1/2023.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeView()
+    }
+}
