1424daa227659c4fb88c07c1cf684886cc126d8d
dwaynediddy <burrows.dwayne@icloud.com>
2023-01-10 11:30:19 +1000

    fixed badge

--- Landmarks/Views/badges/Badge.swift
+++ Landmarks/Views/badges/Badge.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct Badge: View {
-    var BadgeSymbols: some View {
+    var badgeSymbols: some View {
         ForEach(0..<8) { index in
-            RotatedBadgeSymbol(angle: Angle(degrees: 0))
-                .opacity(0.5)
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(index) / Double(8)) * 360.0
+            )
         }
+        .opacity(0.5)
     }
     
     var body: some View {
@@ -20,7 +22,7 @@ struct Badge: View {
             BadgeBackground()
             
             GeometryReader { geometry in
-                BadgeSymbols
+                badgeSymbols
                     .scaleEffect(1.0 / 4.0, anchor: .top)
                     .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
             }
