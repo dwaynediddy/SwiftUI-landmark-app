f25855fb6f81e557222d3ae173292aeac7528cb8
dwaynediddy <burrows.dwayne@icloud.com>
2023-01-17 10:41:29 +0800

    adding profile editor components

---
+++ Landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,49 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by Dwayne on 17/1/2023.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("username").bold()
+                Divider()
+                TextField("username", text: $profile.username)
+            }
+            Toggle(isOn: $profile.prefersNotifications) {
+                Text("Enable notifications").bold()
+            }
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Seasonal Phooto").bold()
+                
+                Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                    ForEach(Profile.Season.allCases) { season in
+                        Text(season.rawValue).tag(season)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            DatePicker(<#LocalizedStringKey#>, selection: $profile.goalDate, in: dateRange, displayedComponents: .date)
+            Text("Goal date").bold()
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
