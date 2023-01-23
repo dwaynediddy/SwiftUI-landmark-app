6a09c688001f9c6707d647316898f52030192703
dwaynediddy <burrows.dwayne@icloud.com>
2023-01-23 15:17:01 +0800

    added pagination with sliding in page view

---
+++ Landmarks/Views/PageView/PageControl.swift
@@ -0,0 +1,27 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Dwayne on 23/1/2023.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+    
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+        
+        return control
+    }
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+    
+}
+
+
